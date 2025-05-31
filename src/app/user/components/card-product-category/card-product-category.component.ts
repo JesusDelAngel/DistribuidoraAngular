aafd864d8c5def9f885e4c0577026cdb6d1a525e
Jesus-Del-Ange <jesusdelangel235@gmail.com>
2025-05-31 03:28:30 -0600

    .

--- src/app/user/components/card-product-category/card-product-category.component.ts
+++ src/app/user/components/card-product-category/card-product-category.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../button/button.component";
+import { HeaderComponent } from "../../../shared/header/header.component";
 
 @Component({
   selector: 'app-card-product-category',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, HeaderComponent],
   templateUrl: './card-product-category.component.html',
   styleUrl: './card-product-category.component.scss'
 })
